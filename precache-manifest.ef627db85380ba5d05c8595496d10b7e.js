5646ce12b60b0a7181ddb1dd0e068895f87f8085
ardiyan15 <ardhiyan15@gmail.com>
2019-09-06 21:55:26 +0700

    initial commit

---
+++ precache-manifest.ef627db85380ba5d05c8595496d10b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbb28dfa86859eb9a2440257be3f043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32818eb56225a964ce2f",
+    "url": "/static/css/main.c4c82e05.chunk.css"
+  },
+  {
+    "revision": "7d0635016e8ea0ba09c0",
+    "url": "/static/js/2.12be56ca.chunk.js"
+  },
+  {
+    "revision": "32818eb56225a964ce2f",
+    "url": "/static/js/main.2987cf86.chunk.js"
+  },
+  {
+    "revision": "d4ba11750aa468b882f9",
+    "url": "/static/js/runtime~main.6e36c75a.js"
+  },
+  {
+    "revision": "eb32f8ea198ac8bf19d7815b89834251",
+    "url": "/static/media/image1.eb32f8ea.jpg"
+  }
+]);
